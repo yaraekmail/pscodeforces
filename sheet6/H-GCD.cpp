f6ab79b639c45d2102a9d3f1e84bbebdd0e452a4
Yarakmail <yaraeyadkmail272@gmail.com>
2026-01-06 01:47:33 +0200

    Add files via upload

---
+++ sheet6/H-GCD.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b)<<endl;
+    cout << lcm(a, b);
+    return 0;
+}
